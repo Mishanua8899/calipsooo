688d9877c48b8eadad9f4e539444a0b7f601c180
Misha <zayzolkov11@gmail.com>
2023-05-01 14:21:21 +0300

    Animation for Notification

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,9 @@
-function init() {
-    alert("Пока нет товара в нaличии")
+let button = document.querySelector('.buy_product');
+let notification = document.querySelector('.notification');
+function OnClick() {
+    button.addEventListener('click', function(e){
+        e.preventDefault()
+        notification.classList.add('move')
+    } )
 }
+OnClick()
